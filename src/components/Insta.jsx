a185ee239068950bcb9ece90153afd462e7f1093
D_water <chrisjerry070@gmail.com>
2023-10-19 01:11:57 +0400

    update

--- src/components/Insta.jsx
+++ src/components/Insta.jsx
@@ -1,52 +1,59 @@
 import React, { useEffect, useState } from 'react'
+import {Swiper, SwiperSlide,} from "swiper/react"
+import 'swiper/css';
 
 
 const Insta = () => {
-    const [show, setShow] = useState(true)
-
-    function displayLink(e) {
-     
-    }
+   
   return (
     <>
     <div className="posts">
-           <div className="postflex">
+        <div className="postflex">
+            <Swiper
+               speed={400}
+               loop= {true}
+               autoplay={{ delay: 1000 }}
+                slidesPerView={4}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
 
-                <div className="post">
+                <SwiperSlide className="post">
                     <img  src="https://preview.colorlib.com/theme/jackco/assets/img/gallery/gallery1.jpg" alt="post" /> 
-                   {show && <div className="imgLink 1">
+                    <div className="imgLink 1">
                         <a href="https://preview.colorlib.com/theme/jackco/assets/img/gallery/gallery1.jpg"><i className="fa-solid fa-magnifying-glass-plus"></i></a>
-                    </div> }
-                </div>
+                    </div> 
+                </SwiperSlide>
 
-                <div className="post" >
-                    <img onMouseEnter={displayLink} src="https://preview.colorlib.com/theme/jackco/assets/img/gallery/gallery2.jpg" alt="post" />
-                    {show && <div className="imgLink 2">
+                <SwiperSlide className="post" >
+                    <img  src="https://preview.colorlib.com/theme/jackco/assets/img/gallery/gallery2.jpg" alt="post" />
+                    <div className="imgLink 2">
                         <a href="https://preview.colorlib.com/theme/jackco/assets/img/gallery/gallery2.jpg"><i className="fa-solid fa-magnifying-glass-plus"></i></a>
-                    </div> }    
-                </div>
+                    </div>    
+                </SwiperSlide>
 
-                <div className="post">
-                    <img onMouseEnter={displayLink} src="https://preview.colorlib.com/theme/jackco/assets/img/gallery/gallery3.jpg" alt="post" />
-                    {show && <div className="imgLink 3">
+                <SwiperSlide className="post">
+                    <img  src="https://preview.colorlib.com/theme/jackco/assets/img/gallery/gallery3.jpg" alt="post" />
+                    <div className="imgLink 3">
                         <a href="https://preview.colorlib.com/theme/jackco/assets/img/gallery/gallery3.jpg"><i className="fa-solid fa-magnifying-glass-plus"></i></a>
-                    </div> }  
-                </div>
+                    </div>  
+                </SwiperSlide>
 
-                <div className="post">
-                    <img onMouseEnter={displayLink} src="https://preview.colorlib.com/theme/jackco/assets/img/gallery/gallery4.jpg" alt="post" />
-                    {show && <div className="imgLink 4">
+                <SwiperSlide className="post">
+                    <img  src="https://preview.colorlib.com/theme/jackco/assets/img/gallery/gallery4.jpg" alt="post" />
+                    <div className="imgLink 4">
                         <a href="https://preview.colorlib.com/theme/jackco/assets/img/gallery/gallery4.jpg"><i className="fa-solid fa-magnifying-glass-plus"></i></a>
-                    </div> }
-                </div>
+                    </div> 
+                </SwiperSlide>
 
-                <div className="post" >
-                    <img onMouseEnter={displayLink} src="https://preview.colorlib.com/theme/jackco/assets/img/gallery/gallery2.jpg" alt="post" />
-                    {show && <div className="imgLink 5">
+                <SwiperSlide className="post" >
+                    <img  src="https://preview.colorlib.com/theme/jackco/assets/img/gallery/gallery2.jpg" alt="post" />
+                    <div className="imgLink 5">
                         <a href="https://preview.colorlib.com/theme/jackco/assets/img/gallery/gallery2.jpg"><i className="fa-solid fa-magnifying-glass-plus"></i></a>
-                    </div> } 
-                </div>
-            </div>
+                    </div> 
+                </SwiperSlide>
+            </Swiper>
+        </div>
 
         </div>
 
