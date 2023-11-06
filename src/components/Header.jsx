0474080bf31f1118820dded89874860c2f6a13eb
D_water <chrisjerry070@gmail.com>
2023-11-07 02:52:17 +0400

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,14 +5,17 @@ import Navbar from "./Navbar"
 const Header = () => {
   return (
     <div  className="head">
-        <div id="header__body">
-            <div className='container'>
-                <h1>web Engineer</h1>
-                <p>Hi, I am Dwater, focusing on creating emotional experiences.</p>
-            </div>
+      <div id="header__body">
+        <div className="header-img">
+          <img src="/src/assets/mine.jpg" alt="image" />
         </div>
-        <div className='hire' >
-            <a href="/" className="hireBtn">HIRE ME</a>
+          <div className='container'>
+              <h1>web Engineer</h1>
+              <p>Hi, I am Dwater, focusing on creating emotional experiences.</p>
+          </div>
+      </div>
+      <div className='hire' >
+          <a href="/" className="hireBtn">HIRE ME</a>
         </div>
     </div>
   )
